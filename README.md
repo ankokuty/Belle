b80d1a95492024c7cb8834a3745d08226ea4179e
Yutaka Kokubu <bun@devnull.jp>
2016-09-29 14:51:52 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# burp_ja
 Burp Suite 非公式日本語化ツール
+====
+
+このツールは、PortSwigger社の製品であるBurp Suiteのインタフェースを、日本語化するツールです。
+
+## 使用方法
+
+### インストーラーを使ってBurpをインストールした場合
+
+burp_ja.jar と burp_ja.txt　の２つのファイルをダウンロードし、Burp Suiteがインストールされているフォルダにコピーします。
+インストール時に変更していなければ、``C:\Program Files\BurpSuiteFree`` や``C:\Program Files\BurpSuitePro``にあると思います。
+
+BurpSuiteFree.vmoptions か BurpSuitePro.vmoptions ファイルをエディタで開き、１行追記します。
+
+```bash
+# Enter one VM parameter per line
+# For example, to adjust the maximum memory usage to 512 MB, uncomment the following line:
+# -Xmx512m
+# To include another file, uncomment the following line:
+# -include-options [path to other .vmoption file]
+-Xmx12151m
+# 以下の行を追記
+-javaagent:burp_ja.jar
+```
+
+通常通り、スタートメニューのショートカット等から起動します。
+
+### Burp Suiteのjarファイルを任意の場所にインストールした場合
+
+burp_ja.jar と burp_ja.txt　の２つのファイルをダウンロードし、Burpのjarファイル(`burpsuite_free_v1.7.06.jar`等)と同じフォルダにコピーします。
+
+Burpのjarファイルのあるフォルダに移動し、(-jar オプションより前に) -javaagenコマンドラインオプションを指定して起動します。
+
+```
+java -javaagent:burp_ja.jar -Xmx1024m -jar burpsuite_free_v1.7.06.jar
+```
+
+## 注意事項
+
+このツールは、私個人が勝手に開発したもので、PortSwigger社は一切関係ありません。日本語訳の間違いはもちろんのこと、不具合等についてPortSwiggerに問い合わせないようお願いします。
+
+このツールは内部でJava実行環境のバイトコードを変更します。Oracle社のJava実行環境で使用した場合[バイナリ・コードライセンス](http://www.oracle.com/technetwork/java/javase/terms/license/index.html)に違反する可能性があります。ライセンスを確認の上、[OpenJDK](http://openjdk.java.net/)等その他のJava実行環境での実行を推奨します。
+
+このツールは単純に、辞書ファイルで指定した文字列にマッチする文字列を、対応する日本語訳に変換しています。
+影響範囲をGUI表示部分に限定しているつもりですが、送受信したHTTPメッセージも意図せず変換してしまっている可能性があります。
+Webアプリケーションテストへの影響を確認の上、自己責任で使用してください。
+
+PortSwigger社から英語メッセージ一覧をもらったわけではなく、使用中に英語表記であることを目視した箇所を適宜翻訳しているため、おそらく網羅されていません。
+未翻訳の箇所を見つけた場合は、ご連絡ください。
+
+## References
+
+- [Burp Suite](https://portswigger.net/burp/)
+- [Burp Suite Japan](https://twitter.com/burpsuitejapan)
+- [Burp Suite Japanユーザグループ　サイボウズLive](https://cybozulive.com/join/request/applyNotLogin?key=R602BpyfCD)
+
+## Author
+
+[@_bun_](https://twitter.com/_bun_)
---
+++ src/jp/devnull/burp_ja/Injection.java
@@ -0,0 +1,125 @@
+package jp.devnull.burp_ja;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+import java.util.ArrayList;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+
+public class Injection {
+	static ClassPool classPool;
+	static ArrayList<Translate> translates;
+
+	static class Translate {
+		String en;
+		String ja;
+
+		/**
+		 * @param en
+		 * @param ja
+		 */
+		public Translate(String[] s) {
+			super();
+			en = s[0];
+			ja = s[1];
+		}
+	}
+
+	public static void premain(String agentArgs, Instrumentation instrumentation) throws Exception{
+		// ファイルから翻訳辞書を読み込む
+		translates = new ArrayList<Translate>();
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream("burp_ja.txt"), "UTF-8"));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			try {
+				translates.add(new Translate(line.split("\t", 2)));
+			} catch (Exception e) {
+				// 無視する
+			}
+		}
+		reader.close();
+
+		classPool = ClassPool.getDefault();
+		instrumentation.addTransformer(new ClassFileTransformer() {
+			String makeCommand(int n) {
+				StringBuilder command = new StringBuilder();
+				command.append("{");
+				command.append(String.format("if($%d!=null && $%d.length()>0){", n, n));
+				// テーブルの中身など一部は翻訳しない
+				command.append(
+						"if (("
+						+ "javax.swing.table.DefaultTableCellRenderer.class.isAssignableFrom($0.getClass())"
+						+ "  && !sun.swing.table.DefaultTableCellHeaderRenderer.class.isAssignableFrom($0.getClass())"
+						+ ") || javax.swing.DefaultListCellRenderer.class.isAssignableFrom($0.getClass())) {} else {");
+
+				for (Translate t : translates) {
+					command.append(String.format("$%d=$%d.replaceAll(\"^"
+						+ t.en.replace("\"", "\\\"").replace("%","%%")+ "$\",\""
+						+ t.ja.replace("\"", "\\\"").replace("%","%%") + "\");", n, n));
+				}
+
+				/*
+				// 翻訳されていない文を標準エラーに出す。
+				command.append(String.format(
+					"if("
+					+ "($%d.getBytes().length) == $%d.length()" // 翻訳されていないもののみ
+					+ " && !$%d.matches(\"https?://.+\")"       // URLを無視
+					+ " && !$%d.matches(\"[0-9,]+\")"           // 数値を無視
+					+ " && !$%d.matches(\"burp\\..*\")"         // burp.から始まるもの(クラス名？)を無視
+					+ " && $%d.length()>1"                      // １文字を無視
+					+ " && !$%d.matches(\"[A-Z]+s?\")"          // １単語のみを無視
+					+ "){System.err.println($%d);}", n,n,n,n,n,n,n,n));
+				*/
+				command.append("}}}");
+				return command.toString();
+			}
+
+			public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+					ProtectionDomain protectionDomain, byte[] classfileBuffer)
+					throws IllegalClassFormatException {
+				try {
+					if (className.equals("java/awt/Frame") || className.equals("java/awt/Dialog")) {
+						CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+						CtMethod ctMethod = ctClass.getDeclaredMethod("setTitle");
+						ctMethod.insertBefore(makeCommand(1));
+						return ctClass.toBytecode();
+					} else if (className.equals("javax/swing/JLabel")
+							|| className.equals("javax/swing/AbstractButton")
+							|| className.equals("javax/swing/text/JTextComponent)")) {
+						CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+						CtMethod ctMethod = ctClass.getDeclaredMethod("setText");
+						ctMethod.insertBefore(makeCommand(1));
+						return ctClass.toBytecode();
+					} else if (className.equals("javax/swing/JTabbedPane")) {
+						CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+						CtMethod ctMethod = ctClass.getDeclaredMethod("addTab");
+						ctMethod.insertBefore(makeCommand(1));
+						ctMethod = ctClass.getDeclaredMethod("insertTab");
+						ctMethod.insertBefore(makeCommand(1));
+						return ctClass.toBytecode();
+					} else if (className.equals("javax/swing/text/AbstractDocument")) {
+						CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+						CtMethod ctMethod = ctClass.getDeclaredMethod("insertString");
+						ctMethod.insertBefore(makeCommand(2));
+						return ctClass.toBytecode();
+					} else {
+						return null;
+					}
+				} catch (Exception ex) {
+					IllegalClassFormatException e = new IllegalClassFormatException();
+					e.initCause(ex);
+					throw e;
+				}
+			}
+		});
+	}
+}
